3143e06d77a6ef2924ef89d00fc7d8944910dabc
Lakamo Samuel <lakamosamuel7@gmail.com>
2026-07-30 03:15:44 +0100

    , .

--- atrixia-frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwidcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [react(), tailwidcss()],
-})
